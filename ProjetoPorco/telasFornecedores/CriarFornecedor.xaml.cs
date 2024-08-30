0333873858ed9ddf9cce742a8c0370f1500d29a0
ProjetoPorco <borelabryan947@gmail.com>
2024-08-30 09:16:14 -0300

    gg

--- ProjetoPorco/telasFornecedores/CriarFornecedor.xaml.cs
+++ ProjetoPorco/telasFornecedores/CriarFornecedor.xaml.cs
@@ -11,26 +11,26 @@ namespace ProjetoPorco
          InitializeComponent();
         }
 
-        private void OnCadastrarClienteClicked (object sender, EventArgs e)
-        {
-             
-               // O código abaixo preenche o objeto cliente (Modelo) com os dados das Entry's
-               var Fornecedor = new Modelos.Fornecedor();
-               if (!String.IsNullOrEmpty(IdLabel.Text))
-                 Fornecedor.id      = int.Parse(IdLabel.Text);
-               else
-                Fornecedor.id       = 0;
-               Fornecedor.Nome      = fNome.Text;
-               Fornecedor.Email     = fEmail.Text;
-               Fornecedor.Telefone  = fTelefone.Text;
-             
-         
-               // Com o objeto preenchido enviamos para o controle para criar/atualizar no Banco de Dados
-                FornecedorCrotroles.CriarOuAtualizar(Fornecedor);
-               // Mostra a mensagem de sucesso
-                  DisplayAlert("Salvar", "Dados salvos com sucesso!", "OK");
-             
-        }
+     // private void OnCadastrarClienteClicked (object sender, EventArgs e)
+     // {
+     //      
+     //        // O código abaixo preenche o objeto cliente (Modelo) com os dados das Entry's
+     //        var Fornecedor = new Modelos.Fornecedor();
+     //        if (!String.IsNullOrEmpty(IdLabel.Text))
+     //          Fornecedor.id      = int.Parse(IdLabel.Text);
+     //        else
+     //         Fornecedor.id       = 0;
+     //        Fornecedor.Nome      = fNome.Text;
+     //        Fornecedor.Email     = fEmail.Text;
+     //        Fornecedor.Telefone  = fTelefone.Text;
+     //      
+     //  
+     //        // Com o objeto preenchido enviamos para o controle para criar/atualizar no Banco de Dados
+     //         FornecedorCrotroles.CriarOuAtualizar(Fornecedor);
+     //        // Mostra a mensagem de sucesso
+     //           DisplayAlert("Salvar", "Dados salvos com sucesso!", "OK");
+     //      
+     // }
     
       private void botaov (object sender, EventArgs args)
      {
